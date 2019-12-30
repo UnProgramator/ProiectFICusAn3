468eae638c759488b258f957714ff303556f13ed
Alexandru Pescaru <apescaru11@gmail.com>
2019-12-30 19:38:08 +0200

    implementat hufapp si lvector dupa cerintele specifice

--- alloc.s
+++ alloc.s
@@ -4,6 +4,9 @@
 .global alloc
 .type global, %function
 
+.global lvector
+.type lvector, %function
+
 .data 
 .balign 4
 stackp: .word 0
@@ -21,17 +24,35 @@ alloc: //;r0 -nr de biti de alocat si rezultatul returnat
     mov r0, #0 //;daca nu mai am memorie suficienta
     b fin
 
-if_not: //am memorie
-    add r2, r0
-    str r2, [r1]
-    ldr r1, =heapspace
-    ldr r3, =stackp
-    ldr r2, [r3]
-    add r1,r1,r2
-    add r2, r0
-    str r2, [r3]
-    mov r0, r1
+    if_not: //am memorie
+        add r2, r0
+        str r2, [r1]
+        ldr r1, =heapspace
+        ldr r3, =stackp
+        ldr r2, [r3]
+        add r1, r1, r2 // &heapspace + stack_top
+        add r2, r2, r0 // stack_top += space_needed
+        str r2, [r3]
+        mov r0, r1
+        
+    fin:
+        bx lr
+//end of alloc
     
-fin:
+lvector:
+    push {lr}
+    // no_start -> r0
+    // no_fin   -> r1
+    
+    push {r0}
+    
+    sub r0, r1, r0 // r0 = no_fin - no_start
+    bl alloc
+    
+    mov r1, r0
+    pop {r0}
+    sub r0, r0, r1 // r0 = r0 - no_start
+    
+    pop {lr}
     bx lr
-    
+//end of lvector
--- hufapp.s
+++ hufapp.s
@@ -6,3 +6,59 @@
 .balign 4
 
 .text 
+hufapp:
+    // in ordine *index -> r0
+    //           *nprob -> r1
+    //            n     -> r2
+    //            i     -> r3
+    //            k     -> r4
+    //            j     -> r5
+    
+    push {lr}
+    ldr r4, [r3, r0] //;k = index[i]
+    
+    mov r8, r2 // r8 <- copy of n
+    rsh r8, #1
+    
+    //;while i <= (n >> 1)
+    while: 
+        cmp r3, r8 // i <= (n >> 1) 
+        bgt end
+        
+        //if (j = i<<1) < n && (nprob[index[j]] > nrpob[index[j+1]])
+        //(j = i<<1) < n
+        mov r5, r3 // j = i
+        lsh r5, #1 // j = i << 1
+        cmp r5, r2 // j < n
+        bge fi1
+        //(nprob[index[j]] > nrpob[index[j+1]])
+        ldr r6, [r0,r5] //index[j]
+        ldr r6, [r1,r6] //nprob[index[j]]
+        add r5, #1 //j++
+        ldr r7, [r0,r5] //index[j+1]
+        ldr r7, [r1,r7] //nprob[index[j+1]]
+        cmp r6, r7
+        bgt fi1 //if both condition are true, then j is already j++
+        
+        //if first statement true and second false => (j++)--
+        // stiu ca e putin ciudat, da mi s-a parut mai interesant sa optimizez codul
+        sub r5, #1
+        fi1: //end of first if
+        
+        // if nprob[k] <= nprob[index[j]]
+        ldr r6, [r1,r4] //r6 <- nprob[k]
+        ldr r7, [r0,r5] //r7 <- index[j]
+        ldr r7, [r1,r7] //r7 <- nprob[index[j]]
+        cmp r6, r7
+        ble end // if statement true then break
+        ldr r7, [r0,r5] //r7 <- index[j]
+        str r7, [r0,r3] //r7 -> index[i]
+        mov r3, r5      //i = j
+        
+    end:
+    
+    str r4, [r0,r3] // index[i] = k
+    
+    pop {lr}
+    bx lr
+//end of hufapp
